7fb20eebd073aa5fb5c660a58127f4edfeb620e3
jdc01 <joao_canteiro@hotmail.com>
2020-02-23 10:15:05 +0000

    Create data_manager.c

---
+++ the_box/data_manager.c
@@ -0,0 +1,5 @@
+/* Once data is added to the libray there is no need to
+			Add it again, just add from library, it saves a lot of time,
+			lets say we have to sell 2º hand products which are diferent which time.
+			If the product is already in the library is just a matter of adding.		
+		*/
