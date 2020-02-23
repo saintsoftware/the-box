20eea6a4ae6125c6f184315232b4b6c147de65f5
jdc01 <joao_canteiro@hotmail.com>
2020-02-23 11:54:52 +0000

    Create ability.c

---
+++ the_box/game_structures/ability.c
@@ -0,0 +1,60 @@
+
+// ========== DECLARATION =================
+
+extern int name;
+extern int number;
+extern int id;
+extern int image;
+extern int 3d_model;
+extern int pos_x;
+extern int pos_y;
+extern int pos_z;
+
+/*  GENERIC VARIABLES */
+
+
+/* CHARACTERS ARRAY INDEX
+Name
+
+*/
+
+
+char characters_interface_index [10][4] = { 
+
+   {"name", "image", "image_extension", "3d_model", "3d_model_etension", }  
+
+   
+};
+
+char characters_interface [10][4] = { 
+
+   {"name", "image", "image_extension", "3d_model", "3d_model_etension", }  
+
+   
+
+char characters_status_index [1][4] = {
+
+	{"strenght", "agility", "endurance", "3d_model", "3d_model_etension", } 
+};
+
+char characters_status [10][4] = { 
+
+   {"", "", 2, 3}  
+
+   
+};
+
+char characters_status_index [1][4] = {
+
+ 
+};
+
+char characters_status [10][4] = { 
+
+   {"", "", 2, 3}  
+
+   
+};
+
+
+
